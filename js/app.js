25855c45b532e395b5fb1f4f795077f215fab834
Mohamed Abdeltawab <67064410+Mohamed-Abdeltawab@users.noreply.github.com>
2020-10-27 04:18:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 const sections = document.querySelector("ul");
 const fragment = document.createDocumentFragment();
+const allSection = document.querySelectorAll("section"); //select all section to use it in event listener 
 /*add for loop to create the list of sections to scroll down to the section that we want*/
-for (let i = 1 ; i < 4; i++) {
+for (let i = 1 ; i <= allSection.length; i++) {
     const list = document.createElement("li"); //create list
     const link = document.createElement("a"); //create link
     link.textContent = "section" +i; //add text to section in the list of navbar 
-    const allSection = document.querySelectorAll("section"); //select all section to use it in event listener 
     link.addEventListener("click", function () {
-        allSection[i].scrollIntoView({
-            behavior: "smooth"
-        });
-        
-    }); //  add event to scroll to the place we want 
+        allSection[i-1].scrollIntoView({behavior: "smooth"});
+     }); //  add event to scroll to the place we want 
     list.appendChild(link);
     fragment.appendChild(list);
     
